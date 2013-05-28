e4dbf7e9106594188031e44e91b81d3bcc6feec9
Louis Dionne <ldionne.2@gmail.com>
2013-05-27 20:49:21 -0400

    Add a sample potentially deadlocking program.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 
 #include "synchronized.hpp"
+#include <iostream>
+#include <thread>
 
 
 synchronized<int> x(10);
 synchronized<int> y(5);
 
 int main() {
+    std::thread t1([&] {
+        x += y;
+    });
 
+    std::thread t2([&] {
+        y += x;
+    });
+
+    t1.join();
+    t2.join();
+
+    std::cout << "x: " << x << "\ny: " << y << std::endl;
 }
